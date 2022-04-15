de93b92fed47369d5e1298f15c9d6a105c538c36
Aimal khan <aimalkhan9247@gmail.com>
2022-04-15 23:36:58 +0500

    Add files via upload
    Let’s say you have an array of 10 elements. Consider the first five elements and find the maximum integer from them. Then consider the last five elements and find the minimum integer from them. Add up the maximum and minimum integer and output the result.

---
+++ min & max addition.asm
@@ -0,0 +1,57 @@
+[org 0x0100]                 ; Aimal  khan ..... p20-0028.......q3
+                              
+jmp Start                  
+max: dw 0
+min: dw 0
+result: dww 0
+data: dw 1,2,3,4,5,6,7,8,9,10            
+
+Start:
+
+    mov CX, 5
+    mov bx, 0 
+    mov ax, [data] 
+    add bx, 2
+    mov dx, [data+bx]
+
+outerloop1:
+
+    cmp ax,dx 
+    jnc maximum
+    mov ax, dx
+
+    maximum: 
+
+        add bx, 2
+        mov dx, [data+bx]
+        dec cx
+        jnz outerloop1
+        mov [max], ax
+    
+;---------------------------------------------------------------------------------------------------------
+        mov cx , 5
+        mov bx , 8
+        mov ax , [data + bx]
+        add bx , 2
+        mov dx , [data + bx +2]
+
+outerloop2:
+        cmp ax , dx
+        jc minimum
+        mov ax, dx
+
+        minimum:
+            add bx , 2
+            mov dx , [data + bx + 2]
+            dec cx
+            jnz outerloop2
+            mov [min], ax
+
+    mov ax , [max]
+    mov bx , [min]
+    add ax , bx
+    mov [result] , ax ; saving the final result
+    
+
+mov ax,0x4c00
+int 0x21
